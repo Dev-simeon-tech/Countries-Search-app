8a971d2d117f798066d3459aec25eb700c894edb
Elebesunu Osereme <elebesunuosereme@gmail.com>
2025-04-13 17:43:31 +0100

    feat: added PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["earth.png"],
+      manifest: {
+        name: "Countries Search app",
+        short_name: "CountriesSearch",
+        description:
+          "This is a web app that have data about countries that a user an search for.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+      },
+    }),
+  ],
 });
